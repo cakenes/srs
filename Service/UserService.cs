5f113cce1b02ddb10bf885b4b81fb69f6f5b9473
cakenes <jyri.kurtakko@gmail.com>
2021-02-25 20:15:02 +0200

    Huge amount of refractoring, css completely redone and everything re-applied to pages

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -9,7 +9,7 @@ namespace Srs {
 
 		// User login
         public Task<Data.ReturnInfo> LoginUserAsync(string name, string password) {
-            if (name == null || password == null) return Task.FromResult(CreateReturn(false, "What are you doing?", "warning"));
+            if (name == null || password == null) return Task.FromResult(CreateReturn(false, "What is happening?", "warning"));
             else if (name.Length < 3) return Task.FromResult(CreateReturn(false, "Incorrect username", "warning"));
             else if (password.Length < 3) return Task.FromResult(CreateReturn(false, "Incorrect password", "warning"));
             Data.ReturnInfo info = Access.Current.LoginUser(name,password);
@@ -26,7 +26,7 @@ namespace Srs {
 
         // Create user
         public Task<Data.ReturnInfo> CreateUserAsync(string name, string password) {
-            if (name == null || password == null) return Task.FromResult(CreateReturn(false, "What are you doing?", "warning"));
+            if (name == null || password == null) return Task.FromResult(CreateReturn(false, "What is happening?", "warning"));
             else if (name.Length < 3) return Task.FromResult(CreateReturn(false, "Username too short", "warning"));
             else if (password.Length < 3) return Task.FromResult(CreateReturn(false, "Password too short", "warning"));
             Data.ReturnInfo info = Access.Current.CreateUser(name,password);
